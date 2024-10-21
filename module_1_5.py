d99339e15724e3dca0d0e0bd8dc7c8a7d24df2b1
8Evgenii8 <jiganpro77@gmail.com>
2024-10-21 10:25:16 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, 'a', 'b'
+print(immutable_var)
+# immutable_var_[0] = 200
+# Кортеж не возможно поменять.
+mutable_list = [1, 2, 'a', 'b', 'apple'],
+print(mutable_list)
+mutable_list [0][4] = 'c'
+print(mutable_list)
